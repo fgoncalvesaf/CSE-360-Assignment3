4edc8a44854e679553c08d2cccc71a59ad63c573
fgoncalvesaf <fgoncalvesaf@gmail.com>
2016-02-21 23:43:33 -0700

    First Commit

---
+++ Calculator.java
@@ -0,0 +1,71 @@
+package cse360assign3;
+
+/**
+* Arizona State University
+* CSE 360 - SOFTWARE ENGINEERING
+* Assignment #: 3
+* @author Francisco Goncalves de Almeida Filho
+* @version 02.21.2016
+* PIN: 321
+* StudentID: 1210128241
+* 
+* Description: This class simulate a calculator.
+*/
+
+public class Calculator
+{
+private int total;
+	
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * This method return the total value.
+	 * @return total
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * This method add a value to the total.
+	 * @param value
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * This method subtract a value to the total.
+	 * @param value
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * This method multiply a value to the total.
+	 * @param value
+	 */
+	public void multiply (int value) {
+		
+	}
+	
+	/**
+	 * This method divide the total by a value.
+	 * @param value
+	 */
+	public void divide (int value) {
+		
+	}
+	
+	/**
+	 * This method shows the history of the results.
+	 * @return history
+	 */
+	public String getHistory () {
+		return "";
+	}
+	
+}
---
+++ CalculatorTest.java
@@ -0,0 +1,78 @@
+package cse360assign3;
+
+/**
+* Arizona State University
+* CSE 360 - SOFTWARE ENGINEERING
+* Assignment #: 3
+* @author Francisco Goncalves de Almeida Filho
+* @version 02.21.2016
+* PIN: 321
+* StudentID: 1210128241
+* 
+* Description: This class tests the Calculator class.
+*/
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@Test
+	/**
+	 * This method verify if the constructor does not return null
+	 */
+	public void testCalculator(){
+		assertNotNull(new Calculator());
+	}
+	
+	@Test
+	/**
+	 *
+	 * This method verify if the total is correct
+	 */
+	public void testTotal(){
+		fail();
+	}
+	
+	@Test
+	/**
+	 * This method verify if a value is been added correctly
+	 */
+	public void testAdd(){
+		fail();
+	}
+	
+	@Test
+	/**
+	 * This method verify if a value is been subtracted correctly
+	 */
+	public void testSubtract(){
+		fail();
+	}
+	
+	@Test
+	/**
+	 * This method verify if the total is been multiplied correctly
+	 */
+	public void testMultiply(){
+		fail();
+	}
+	
+	@Test
+	/**
+	 * This method verify if the total is been divided correctly
+	 */
+	public void testDivide(){
+		fail();
+	}
+	
+	@Test
+	/**
+	 * This method verify if the history is been shown correctly
+	 */
+	public void testHistory(){
+		fail();
+	}
+
+}
